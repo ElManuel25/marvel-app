6042f90bc40fd33d515f7bc55d729032d859fc0f
Emmanuel Velez <emmanuel.velezme@gmail.com>
2023-09-26 16:11:26 -0500

    display info in card

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -1,14 +1,45 @@
 <template>
     <div class="character-card" @click="showDetails">
-        <div class="character-image-container">
-            <img v-if="character.thumbnail" :src="character.thumbnail.path + '.' + character.thumbnail.extension" alt=""
-                class="character-image" />
-        </div>
-        <div class="character-name">
-            {{ character.name }}
-        </div>
+        <center>
+            <div class="character-image-container">
+                <img v-if="character.thumbnail" :src="character.thumbnail.path + '.' + character.thumbnail.extension" alt=""
+                    class="character-image" />
+            </div>
+            <div class="character-name">{{ character.name }}</div>
+            <p class="character-description">{{ character.description || "No description available" }}</p>
+            <div class="character-stats">
+                <div class="stat">
+                    <h3>Comics</h3>
+                    <p class="stat-value">{{ character.comics.available }}</p>
+                </div>
+                <div class="stat">
+                    <h3>Series</h3>
+                    <p class="stat-value">{{ character.series.available }}</p>
+                </div>
+                <div class="stat">
+                    <h3>Stories</h3>
+                    <p class="stat-value">{{ character.stories.available }}</p>
+                </div>
+                <div class="stat">
+                    <h3>Events</h3>
+                    <p class="stat-value">{{ character.events.available }}</p>
+                </div>
+            </div>
+        </center>
+        <h3 align="center" class="first-three-series-title">First three series:</h3>
+        <ul class="first-three-series">
+            <template v-if="character.series.items && character.series.items.length > 0">
+                <li v-for="(seriesItem, index) in character.series.items.slice(0, 3)" :key="seriesItem.resourceURI">
+                    {{ seriesItem.name }}
+                </li>
+            </template>
+            <template v-else>
+                <li>Not in any series</li>
+            </template>
+        </ul>
     </div>
 </template>
+
   
 <script>
 import { ref } from 'vue';
@@ -17,14 +48,27 @@ const character = ref(null);
 const isClicked = ref(false);
 
 const showDetails = () => {
-  console.log('Mostrando detalles de:', character.value.name);
-  isClicked.value = !isClicked.value; // Cambiamos el estado de clic
+    console.log('Mostrando detalles de:', character.value.name);
+    isClicked.value = !isClicked.value;
 }
 
 export { character, showDetails, isClicked };
 
 export default {
-  props: ['character'],
+    props: ['character'],
+    setup() {
+        const firstThreeSeries = ref([]);
+
+        // Lógica para obtener las primeras tres series
+        if (character.value && character.value.series && character.value.series.items) {
+            firstThreeSeries.value = character.value.series.items.slice(0, 3);
+        }
+
+        return {
+            firstThreeSeries,
+            isClicked,
+        };
+    },
 };
 </script>
   
@@ -33,26 +77,20 @@ export default {
     background-color: #fff;
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    padding: 10px;
+    padding: 20px;
     text-align: center;
     cursor: pointer;
     margin-bottom: 20px;
     border: 2px solid #333;
-    margin-top: 20px;
-    /* Añadido: separación del borde superior */
-    margin-left: 20px;
-    /* Añadido: separación del borde izquierdo */
-    margin-right: 20px;
-    /* Añadido: separación del borde derecho */
     transition: transform 0.2s;
 }
 
 .character-image-container {
-    width: 300px;
+    width: 100%;
     height: 300px;
-    margin: 0 auto 10px;
     overflow: hidden;
     border-radius: 10px;
+    margin-bottom: 10px;
 }
 
 .character-image {
@@ -62,20 +100,70 @@ export default {
 }
 
 .character-name {
+    font-size: 1.5em;
     font-weight: bold;
+    margin-bottom: 10px;
+}
+
+.character-description {
+    font-size: 1.1em;
+    color: #555;
+    margin-bottom: 15px;
+    text-align: justify;
+}
+
+.character-stats {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+
+.stat {
+    margin-bottom: 20px;
+    padding: 10px;
+    background-color: #f0f0f0;
+    border-radius: 10px;
+}
+
+.stat h3 {
+    margin: 0;
+    font-size: 20px;
     color: #333;
+}
+
+.stat-value {
+    font-size: 24px;
+    font-weight: bold;
+    color: #e40f0f;
+}
+
+.first-three-series-title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-top: 20px;
+    color: #333;
+}
+
+.first-three-series {
+    list-style: none;
+    padding: 0;
+    font-size: 20px;
+    color: #333;
+}
+
+.first-three-series li {
+    margin-bottom: 10px;
+    color: #680000;
 }
 
 .character-card:hover {
     transform: scale(1.05);
-    /* Escalamos la tarjeta cuando el cursor está encima */
     cursor: pointer;
-    /* Cambiamos el cursor a una mano para indicar que se puede hacer clic */
 }
 
 .character-card.clicked {
-  transform: scale(1.1); /* Escalamos un poco más cuando se hace clic */
-  border: 2px solid #333; /* Añadimos un borde oscuro cuando se hace clic */
+    transform: scale(1.1);
+    border: 2px solid #333;
 }
 </style>
   
